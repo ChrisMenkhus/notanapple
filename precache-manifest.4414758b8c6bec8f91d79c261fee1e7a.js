137fa51124fbee6327697feb9a5161d6fa6c800f
Chris Menkhus <menkhus.chris@gmail.com>
2020-07-18 22:38:42 -0600

    Updates

---
+++ precache-manifest.4414758b8c6bec8f91d79c261fee1e7a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59eee14f740c5cb42f26f9521bdc553",
+    "url": "/notanapple/index.html"
+  },
+  {
+    "revision": "6c5381342e60bae6bb03",
+    "url": "/notanapple/static/css/main.8d3dfc3a.chunk.css"
+  },
+  {
+    "revision": "b5094b22a0a8c683b0d9",
+    "url": "/notanapple/static/js/2.1ec34b83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/notanapple/static/js/2.1ec34b83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c5381342e60bae6bb03",
+    "url": "/notanapple/static/js/main.f1f8a0c2.chunk.js"
+  },
+  {
+    "revision": "7000039dc323f134f4dc",
+    "url": "/notanapple/static/js/runtime-main.b3ff0815.js"
+  },
+  {
+    "revision": "cea062652a577d697bc8bf2d3cadf1e4",
+    "url": "/notanapple/static/media/fruit.cea06265.svg"
+  },
+  {
+    "revision": "0c02a7e085e05d51d5824435214cd911",
+    "url": "/notanapple/static/media/mango.0c02a7e0.png"
+  },
+  {
+    "revision": "237685218307661a71ed3a4c619a502b",
+    "url": "/notanapple/static/media/sliced.23768521.jpg"
+  },
+  {
+    "revision": "624805a585bc9617ad199e59a7df6976",
+    "url": "/notanapple/static/media/wave.624805a5.svg"
+  }
+]);
